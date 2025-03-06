8cd5a53a4b6e08b8c93c90dda9edf2be8a1a1bc4
Meltasy <119603631+Meltasy@users.noreply.github.com>
2025-03-06 17:41:39 +0100

    Rewrite basic information site in Express

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'index.html'))
+})
+app.get('/about', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'about.html'))
+})
+app.get('/contact-me', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'contact-me.html'))
+})
+app.get('*', (req, res) => {
+  res.status(400).sendFile(path.join(__dirname, '404.html'))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Express server funning on port ${PORT}!`)
+})
